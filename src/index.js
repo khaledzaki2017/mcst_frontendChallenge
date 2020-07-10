5c9005d2f923ded60ad7c083a5b3183ab919a25e
khaledzaki2017 <khaledzaky07@gmail.com>
2020-07-10 22:44:18 +0200

    add rootreducer to store

--- src/index.js
+++ src/index.js
@@ -2,76 +2,19 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider, connect} from 'react-redux';
-import {createStore} from 'redux';
-import Cookies from 'js-cookie';
-import Frame from './views/Frame';
-import Login from './views/Login';
-import Welcome from './views/Welcome';
-import auth from './reducers/auth';
-import {login} from './actions/auth';
-let store = createStore(auth, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-class Base extends React.Component{
-  constructor(props){
-    super(props)   
-    this.state = {
-      showLogin: true,
-      showWelcome: true
-    } 
-  }
-  componentWillReceiveProps(nextProps){
-    if(nextProps.login){
-      this.setState({showWelcome: false, showLogin: false});
-    }else{
-      this.setState({showWelcome: false, showLogin: true});      
-    }
-  }
-  componentWillMount(){
-    let token = Cookies.get("token");
-    if(!token){
-      this.setState({showWelcome: false})
-      this.props.setlogin(false);
-      return;
-    }
-    let data = {token: token};
-    // ask the server if the token is valid
-    fetch("http://localhost:3001/login", {method: "POST",
-      body: JSON.stringify(data), 
-      headers: new Headers({
-      'Content-Type': 'application/json'})
-      }).then(response => response.json()).then( res => {
-      if(res.result === "true"){
-        this.setState({showWelcome: false, showLogin: false});
-        this.props.setlogin(true);        
-      } else {
-        this.setState({showWelcome: false, showLogin: true});
-        this.props.setlogin(false);        
-      }
-    })
-  }
-  render(){
-    return (<div style={{height: "100%"}}>
-      {this.state.showWelcome? <Welcome/>: this.state.showLogin? <Login {...this.props}/>: <Frame {...this.props}/>}
-    </div>)
-  }
-}
-const mapStateToProps = (state, ownProps) => {
-  return {
-    login: state.login
-  }
-}
-const mapDispatchToProps = dispatch => {
-  return {
-      setlogin: (flag) => {
-          dispatch(login(flag))
-      }
-  }
-}
-const BaseContainer = connect(mapStateToProps, mapDispatchToProps)(Base);
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import Index from './App'
+import * as allReducers from './reducers/redContainer';
+
+const rootReducer = combineReducers(allReducers)
+
+let store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 document.getElementById('root').style.height = "100%";
 ReactDOM.render(
   <Provider store={store}>
-    <BaseContainer/>
+    <Index />
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
