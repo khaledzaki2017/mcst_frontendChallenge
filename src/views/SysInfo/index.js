9a1bdbd9a46ae50ad08e3a2f15be9f383a858798
khaledzaki2017 <khaledzaky07@gmail.com>
2020-07-10 22:52:55 +0200

    get data and render charts

--- src/views/SysInfo/index.js
+++ src/views/SysInfo/index.js
@@ -1,10 +1,46 @@
 import React from "react";
-export default class SysInfo extends React.Component{
-    render(){
+import axios from "axios"
+import Charts from "../Charts/index"
+import { setData } from '../../actions/setData';
+import { connect } from 'react-redux';
+
+class Info extends React.Component {
+
+    settings = () => {
+        const sett = {
+            type: ["MyPieChart", "MyBarChart"],
+        }
+        return sett
+    }
+    getData = () => {
+        return (axios('http://localhost:3001/admin')
+            .then(resp => {
+                const d = resp.data
+                this.props.setData(d)
+            })
+            .catch(error => {
+                console.log(error);
+            }))
+    }
+    render() {
+
         return (
             <div>
-                System info
+                <h2>System info</h2>
+                <Charts settings={this.settings()} />
+                <button onClick={this.getData.bind(this)}>Show Stats</button>
+
             </div>
         )
     }
 }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setData: (d) => {
+            dispatch(setData(d))
+        }
+    }
+}
+const SysInfo = connect(null, mapDispatchToProps)(Info);
+export default SysInfo
