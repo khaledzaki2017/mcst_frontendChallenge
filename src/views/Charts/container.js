0dc21d13b92251c126013ecc26b3a3dff9e4536f
khaledzaki2017 <khaledzaky07@gmail.com>
2020-07-10 22:30:25 +0200

    container

---
+++ src/views/Charts/container.js
@@ -0,0 +1,7 @@
+// import { lazy } from 'react'
+// export const MyBarChart = lazy(() => import('./barchart'));
+// export const MyPieChart = lazy(() => import('./piechart'));
+export { default as MyBarChart } from './types/barchart'
+export { default as MyPieChart } from './types/piechart'
+
+
