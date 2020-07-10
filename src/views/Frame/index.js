08951eb027c99b044ae627ec4cfcdb9d3a9f276c
khaledzaki2017 <khaledzaky07@gmail.com>
2020-07-10 22:34:39 +0200

    fix bug

--- src/views/Frame/index.js
+++ src/views/Frame/index.js
@@ -2,13 +2,20 @@ import NavMenu from '../NavMenu';
 import SysInfo from "../SysInfo";
 import Notifications from "../notifications";
 import React from 'react'
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import { Drawer, Button, Layout, Menu } from 'antd';
+import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
+import { Drawer, Layout } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
 
-const { Header, Content, Sider } = Layout;
+const { Header, Content } = Layout;
 
 export default class Frame extends React.Component {
+    constructor(props) {
+        super(props);
+        if (!this.props.login) {
+            <Redirect to="/login" />
+        }
+    }
     state = { visible: false, placement: 'left' };
 
     showDrawer = () => {
@@ -39,44 +46,32 @@ export default class Frame extends React.Component {
             Open
           </Button> */}
                 <BrowserRouter>
-
-
-
-
-
                     <Layout style={{ height: "100%" }}>
                         <Header className="header">
-                            <div className="logo" ><h1>Logo</h1></div>
-                            {/* <Menu
-                            theme="dark"
-                            mode="horizontal"
-                            defaultSelectedKeys={['2']}
-                            style={{ lineHeight: '64px' }}
-                        >
-                        </Menu> */}
-                            <UserOutlined onClick={this.showDrawer} />
+                            <div className="logo">
+                                <img src="https://www.easyunime.com/media/institution/photo/2019/01/10/ALMAAREFA_UNIVERSITY.jpg.600x400_q85.jpg" id="icon" alt="MCST LOGO" />
+                            </div>
+                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined onClick={this.showDrawer} />} />
                         </Header>
                         <Layout>
-                            <Drawer
+                            <Drawer className="drawer"
                                 title="Admission Male"
                                 placement={placement}
                                 closable={false}
                                 onClose={this.onClose}
                                 visible={visible}
-                                key={placement}>                            <NavMenu {...this.props} />
+                                key={placement}>    <NavMenu  {...this.props} closeDrawer={() => { this.setState({ visible: false }) }} />
                             </Drawer>
                             <Layout style={{ padding: '24px 24px 24px' }}>
                                 <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
                                     <Switch>
-                                        <Route exact path="/" component={SysInfo}></Route>
+                                        <Route path="/dashboard" component={SysInfo}></Route>
                                         <Route path="/notifications" component={Notifications}></Route>
                                     </Switch>
                                 </Content>
                             </Layout>
                         </Layout>
                     </Layout>
-
-
 
                 </BrowserRouter>
 
