ec8844c693074154029fae7c74cb9b9f9cd08ee8
khaledzaki2017 <khaledzaky07@gmail.com>
2020-07-10 22:40:33 +0200

    modif

--- json_server.js
+++ json_server.js
@@ -1,25 +1,24 @@
-
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
- 
+
 // custom routes before JSON Server router
 server.get('/echo', (req, res) => {
   res.jsonp(req.query)
 })
- 
+
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     console.log(req.body);
-    if(req.body.username == "admin" && req.body.pwd == "123456" || req.body.token == "123456"){
-        const result = {result: "true", token: "123456"};
-        res.jsonp(result);
-        return;
+    if (req.body.username == "admin" && req.body.pwd == "123456" || req.body.token == "123456") {
+      const result = { result: "true", role: "admin", token: "123456" };
+      res.jsonp(result);
+      return;
     }
   }
   // Continue to JSON Server router
