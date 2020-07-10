85c990ffb034aaee0e01eff8a4c262d6c16be392
khaledzaki2017 <khaledzaky07@gmail.com>
2020-07-10 22:49:48 +0200

    enhance app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import { BrowserRouter, Route } from 'react-router-dom';
+import Main from './route/index';
+class Index extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Route component={Main} />
+      </BrowserRouter>
     );
   }
 }
-
-export default App;
+export default Index
